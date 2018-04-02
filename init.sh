15bbe63a975c6c76dcb25a55a54d6b242f536b28
Tyler Lubeck <tyler@tylerlubeck.com>
2018-04-02 10:55:02 -0700

    Dynamically create iterm2 profile at init time

--- init.sh
+++ init.sh
@@ -4,5 +4,10 @@ pushd $MYPATH
 echo "install color-italic terminfo..."
 tic -s ./init/xterm-256color-italic.terminfo
 tic -s ./init/screen-256color-italic.terminfo
+
+echo "Installing iterm2 profile..."
+# Replaces the working directory based on user account
+cat ./init/iterm2.profile.tmpl| jq ".[\"Working Directory\"] = \"$HOME\""  > ~/Library/Application\ Support/iTerm2/DynamicProfiles/falcon
+
 echo
 popd
